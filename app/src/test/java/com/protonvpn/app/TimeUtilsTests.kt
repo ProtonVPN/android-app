406b036ecf853ff7bd3a6d234c355bb723d603b1
Mateusz Markowicz <poniekad@protonmail.com>
2020-01-07 12:32:33 +0100

    Fix getFormattedTimeFromSeconds and add unit test

---
+++ app/src/test/java/com/protonvpn/app/TimeUtilsTests.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2019 Proton Technologies AG
+ * 
+ * This file is part of ProtonVPN.
+ * 
+ * ProtonVPN is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ProtonVPN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.protonvpn.app
+
+import com.protonvpn.android.utils.TimeUtils.getFormattedTimeFromSeconds
+import org.junit.Assert
+import org.junit.Test
+
+class TimeUtilsTests {
+
+    @Test
+    fun testPeriodFormatter() {
+        Assert.assertEquals("0s", getFormattedTimeFromSeconds(0))
+        Assert.assertEquals("12s", getFormattedTimeFromSeconds(12))
+        Assert.assertEquals("1m", getFormattedTimeFromSeconds(60))
+        Assert.assertEquals("3h 5s", getFormattedTimeFromSeconds(3*60*60 + 5))
+    }
+}
