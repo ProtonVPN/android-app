6e8ff28768f1635918937da95e98df1ec347a1e0
Algirdas Pundzius <algirdas.pundzius@protonmail.com>
2020-02-25 11:51:55 +0200

    Move countries to strings.xml to be translatable

--- app/src/main/java/com/protonvpn/android/utils/CountryTools.java
+++ app/src/main/java/com/protonvpn/android/utils/CountryTools.java
@@ -20,12 +20,13 @@ package com.protonvpn.android.utils;
 
 import android.content.Context;
 
+import com.protonvpn.android.ProtonApplication;
+
 import java.util.HashMap;
 
 public class CountryTools {
 
     public static HashMap<String, Double> locationMap;
-    private static HashMap<String, String> countryNamingMap;
 
     static {
         locationMap = new HashMap<String, Double>() {{
@@ -160,261 +161,6 @@ public class CountryTools {
         }};
     }
 
-    static {
-        countryNamingMap = new HashMap<String, String>() {{
-            put("AF", "Afghanistan");
-            put("AX", "Aland Islands");
-            put("AL", "Albania");
-            put("DZ", "Algeria");
-            put("AS", "American Samoa");
-            put("AD", "Andorra");
-            put("AO", "Angola");
-            put("AI", "Anguilla");
-            put("AQ", "Antarctica");
-            put("AG", "Antigua and Barbuda");
-            put("AR", "Argentina");
-            put("AM", "Armenia");
-            put("AW", "Aruba");
-            put("AU", "Australia");
-            put("AT", "Austria");
-            put("AZ", "Azerbaijan");
-            put("BS", "Bahamas");
-            put("BH", "Bahrain");
-            put("BD", "Bangladesh");
-            put("BB", "Barbados");
-            put("BY", "Belarus");
-            put("BE", "Belgium");
-            put("BZ", "Belize");
-            put("BJ", "Benin");
-            put("BM", "Bermuda");
-            put("BT", "Bhutan");
-            put("BO", "Bolivia");
-            put("BQ", "Bonaire");
-            put("BA", "Bosnia and Herzegovina");
-            put("BW", "Botswana");
-            put("BV", "Bouvet Island");
-            put("BR", "Brazil");
-            put("IO", "British Indian Ocean Territory");
-            put("BN", "Brunei Darussalam");
-            put("BG", "Bulgaria");
-            put("BF", "Burkina Faso");
-            put("BI", "Burundi");
-            put("KH", "Cambodia");
-            put("CM", "Cameroon");
-            put("CA", "Canada");
-            put("CV", "Cape Verde");
-            put("KY", "Cayman Islands");
-            put("CF", "Central African Republic");
-            put("TD", "Chad");
-            put("CL", "Chile");
-            put("CN", "China");
-            put("CX", "Christmas Island");
-            put("CC", "Cocos Islands");
-            put("CO", "Colombia");
-            put("KM", "Comoros");
-            put("CG", "Congo");
-            put("CD", "Congo DR");
-            put("CK", "Cook Islands");
-            put("CR", "Costa Rica");
-            put("CI", "Côte d'Ivoire");
-            put("HR", "Croatia");
-            put("CU", "Cuba");
-            put("CW", "Curaçao");
-            put("CY", "Cyprus");
-            put("CZ", "Czechia");
-            put("DK", "Denmark");
-            put("DJ", "Djibouti");
-            put("DM", "Dominica");
-            put("DO", "Dominican Republic");
-            put("EC", "Ecuador");
-            put("EG", "Egypt");
-            put("SV", "El Salvador");
-            put("GQ", "Equatorial Guinea");
-            put("ER", "Eritrea");
-            put("EE", "Estonia");
-            put("ET", "Ethiopia");
-            put("FK", "Falkland Islands");
-            put("FO", "Faroe Islands");
-            put("FJ", "Fiji");
-            put("FI", "Finland");
-            put("FR", "France");
-            put("GF", "French Guiana");
-            put("PF", "French Polynesia");
-            put("TF", "French Southern Territories");
-            put("GA", "Gabon");
-            put("GM", "Gambia");
-            put("GE", "Georgia");
-            put("DE", "Germany");
-            put("GH", "Ghana");
-            put("GI", "Gibraltar");
-            put("GR", "Greece");
-            put("GL", "Greenland");
-            put("GD", "Grenada");
-            put("GP", "Guadeloupe");
-            put("GU", "Guam");
-            put("GT", "Guatemala");
-            put("GG", "Guernsey");
-            put("GN", "Guinea");
-            put("GW", "Guinea-Bissau");
-            put("GY", "Guyana");
-            put("HT", "Haiti");
-            put("HM", "Heard Island and McDonald Islands");
-            put("VA", "Holy See (Vatican City State)");
-            put("HN", "Honduras");
-            put("HK", "Hong Kong");
-            put("HU", "Hungary");
-            put("IS", "Iceland");
-            put("IN", "India");
-            put("ID", "Indonesia");
-            put("IR", "Iran");
-            put("IQ", "Iraq");
-            put("IE", "Ireland");
-            put("IM", "Isle of Man");
-            put("IL", "Israel");
-            put("IT", "Italy");
-            put("JM", "Jamaica");
-            put("JP", "Japan");
-            put("JE", "Jersey");
-            put("JO", "Jordan");
-            put("KZ", "Kazakhstan");
-            put("KE", "Kenya");
-            put("KI", "Kiribati");
-            put("KP", "North Korea");
-            put("KR", "South Korea");
-            put("KW", "Kuwait");
-            put("KG", "Kyrgyzstan");
-            put("LA", "Lao People's Democratic Republic");
-            put("LV", "Latvia");
-            put("LB", "Lebanon");
-            put("LS", "Lesotho");
-            put("LR", "Liberia");
-            put("LY", "Libya");
-            put("LI", "Liechtenstein");
-            put("LT", "Lithuania");
-            put("LU", "Luxembourg");
-            put("MO", "Macao");
-            put("MK", "Macedonia");
-            put("MG", "Madagascar");
-            put("MW", "Malawi");
-            put("MY", "Malaysia");
-            put("MV", "Maldives");
-            put("ML", "Mali");
-            put("MT", "Malta");
-            put("MH", "Marshall Islands");
-            put("MQ", "Martinique");
-            put("MR", "Mauritania");
-            put("MU", "Mauritius");
-            put("YT", "Mayotte");
-            put("MX", "Mexico");
-            put("FM", "Micronesia");
-            put("MD", "Moldova");
-            put("MC", "Monaco");
-            put("MN", "Mongolia");
-            put("ME", "Montenegro");
-            put("MS", "Montserrat");
-            put("MA", "Morocco");
-            put("MZ", "Mozambique");
-            put("MM", "Myanmar");
-            put("NA", "Namibia");
-            put("NR", "Nauru");
-            put("NP", "Nepal");
-            put("NL", "Netherlands");
-            put("NC", "New Caledonia");
-            put("NZ", "New Zealand");
-            put("NI", "Nicaragua");
-            put("NE", "Niger");
-            put("NG", "Nigeria");
-            put("NU", "Niue");
-            put("NF", "Norfolk Island");
-            put("MP", "Northern Mariana Islands");
-            put("NO", "Norway");
-            put("OM", "Oman");
-            put("PK", "Pakistan");
-            put("PW", "Palau");
-            put("PS", "Palestinian Territory");
-            put("PA", "Panama");
-            put("PG", "Papua New Guinea");
-            put("PY", "Paraguay");
-            put("PE", "Peru");
-            put("PH", "Philippines");
-            put("PN", "Pitcairn");
-            put("PL", "Poland");
-            put("PT", "Portugal");
-            put("PR", "Puerto Rico");
-            put("QA", "Qatar");
-            put("RE", "Réunion");
-            put("RO", "Romania");
-            put("RU", "Russia");
-            put("RW", "Rwanda");
-            put("BL", "Saint Barthélemy");
-            put("SH", "Saint Helena");
-            put("KN", "Saint Kitts and Nevis");
-            put("LC", "Saint Lucia");
-            put("MF", "Saint Martin");
-            put("PM", "Saint Pierre and Miquelon");
-            put("VC", "Saint Vincent and the Grenadines");
-            put("WS", "Samoa");
-            put("SM", "San Marino");
-            put("ST", "Sao Tome and Principe");
-            put("SA", "Saudi Arabia");
-            put("SN", "Senegal");
-            put("RS", "Serbia");
-            put("SC", "Seychelles");
-            put("SL", "Sierra Leone");
-            put("SG", "Singapore");
-            put("SX", "Sint Maarten");
-            put("SK", "Slovakia");
-            put("SI", "Slovenia");
-            put("SB", "Solomon Islands");
-            put("SO", "Somalia");
-            put("ZA", "South Africa");
-            put("GS", "South Georgia and the South Sandwich Islands");
-            put("ES", "Spain");
-            put("LK", "Sri Lanka");
-            put("SD", "Sudan");
-            put("SR", "Suriname");
-            put("SS", "South Sudan");
-            put("SJ", "Svalbard and Jan Mayen");
-            put("SZ", "Swaziland");
-            put("SE", "Sweden");
-            put("CH", "Switzerland");
-            put("SY", "Syrian Arab Republic");
-            put("TW", "Taiwan");
-            put("TJ", "Tajikistan");
-            put("TZ", "Tanzania");
-            put("TH", "Thailand");
-            put("TL", "Timor-Leste");
-            put("TG", "Togo");
-            put("TK", "Tokelau");
-            put("TO", "Tonga");
-            put("TT", "Trinidad and Tobago");
-            put("TN", "Tunisia");
-            put("TR", "Turkey");
-            put("TM", "Turkmenistan");
-            put("TC", "Turks and Caicos Islands");
-            put("TV", "Tuvalu");
-            put("UG", "Uganda");
-            put("UA", "Ukraine");
-            put("AE", "United Arab Emirates");
-            put("GB", "United Kingdom");
-            put("UK", "United Kingdom");
-            put("US", "United States");
-            put("UM", "USA Islands");
-            put("UY", "Uruguay");
-            put("UZ", "Uzbekistan");
-            put("VU", "Vanuatu");
-            put("VE", "Venezuela");
-            put("VN", "Viet Nam");
-            put("VG", "Virgin Islands, British");
-            put("VI", "Virgin Islands, U.S.");
-            put("WF", "Wallis and Futuna");
-            put("EH", "Western Sahara");
-            put("YE", "Yemen");
-            put("ZM", "Zambia");
-            put("ZW", "Zimbabwe");
-        }};
-    }
-
     public static int getFlagResource(Context context, String flag) {
         int desiredFlag = context.getResources()
             .getIdentifier(flag.toLowerCase() + "_flag", "drawable", context.getPackageName());
@@ -423,10 +169,15 @@ public class CountryTools {
     }
 
     public static String getFullName(String country) {
-        return countryNamingMap.get(country) == null ? "Unknown" : countryNamingMap.get(country);
-    }
-
-    private static void initMap() {
-
+        Context context = ProtonApplication.getAppContext();
+        int countryResource = context.getResources()
+            .getIdentifier("country" + country.toUpperCase(), "string", context.getPackageName());
+        if (countryResource > 0) {
+            return context.getString(countryResource);
+        }
+        else {
+            Log.e("Unrecognized country: " + country);
+            return country;
+        }
     }
 }
