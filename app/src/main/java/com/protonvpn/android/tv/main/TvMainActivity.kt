c4f07391116c075cbc16806f792e0aba5e158188
Algirdas Pundzius <algirdas.pundzius@protonmail.com>
2020-12-17 16:16:48 +0200

    Add trial screen popup for Android TV [VPNAND-291]

--- app/src/main/java/com/protonvpn/android/tv/main/TvMainActivity.kt
+++ app/src/main/java/com/protonvpn/android/tv/main/TvMainActivity.kt
@@ -29,8 +29,10 @@ import com.protonvpn.android.R
 import com.protonvpn.android.components.BaseTvActivity
 import com.protonvpn.android.components.ContentLayout
 import com.protonvpn.android.databinding.ActivityTvMainBinding
+import com.protonvpn.android.tv.TvTrialDialogActivity
 import com.protonvpn.android.tv.TvLoginActivity
 import com.protonvpn.android.tv.TvMainFragment
+import com.protonvpn.android.utils.AndroidUtils.launchActivity
 import com.protonvpn.android.utils.CountryTools
 import javax.inject.Inject
 
@@ -63,6 +65,9 @@ class TvMainActivity : BaseTvActivity<ActivityTvMainBinding>() {
             startActivity(Intent(this, TvLoginActivity::class.java))
         }
 
+        if (viewModel.shouldShowTrialDialog()) {
+            launchActivity<TvTrialDialogActivity>()
+        }
         viewModel.onViewInit(lifecycle)
     }
 
