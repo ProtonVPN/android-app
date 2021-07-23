28a04c7f1d7f907c0342cb31b637ba141574c9c9
Marcin Simonides <msimonides@protonmail.com>
2021-07-23 12:18:54 +0200

    Connect to a fallback profile if default is inaccessible [VPNAND-559].

--- app/src/main/java/com/protonvpn/android/vpn/ikev2/ProtonCharonVpnService.kt
+++ app/src/main/java/com/protonvpn/android/vpn/ikev2/ProtonCharonVpnService.kt
@@ -115,6 +115,6 @@ class ProtonCharonVpnService : CharonVpnService() {
 
     private fun handleAlwaysOn() {
         Log.i("[IKEv2] handle always on")
-        vpnConnectionManager.connect(this, manager.defaultConnection, "always-on")
+        vpnConnectionManager.connect(this, manager.defaultAvailableConnection, "always-on")
     }
 }
