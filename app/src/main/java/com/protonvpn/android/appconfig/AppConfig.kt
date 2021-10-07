9335c563c2cd12eedfe79cc3c1483364c272e36b
Marcin Simonides <marcin.simonides@proton.ch>
2021-10-07 13:28:15 +0200

    Fix AppConfig fetch on login.
    AppConfig.update() needs to launch the update in the main scope to avoid
    it being cancelled (as is the case when it runs in a view scope).

--- app/src/main/java/com/protonvpn/android/appconfig/AppConfig.kt
+++ app/src/main/java/com/protonvpn/android/appconfig/AppConfig.kt
@@ -28,10 +28,11 @@ import com.protonvpn.android.utils.ReschedulableTask
 import com.protonvpn.android.utils.Storage
 import com.protonvpn.android.utils.jitterMs
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import me.proton.core.network.domain.ApiResult
 import java.util.concurrent.TimeUnit
 
-class AppConfig(scope: CoroutineScope, val api: ProtonApiRetroFit, val userData: UserData) {
+class AppConfig(private val scope: CoroutineScope, val api: ProtonApiRetroFit, val userData: UserData) {
 
     private var appConfigResponseObservable: MutableLiveData<AppConfigResponse>
 
@@ -43,7 +44,7 @@ class AppConfig(scope: CoroutineScope, val api: ProtonApiRetroFit, val userData:
     val apiNotifications get() = apiNotificationsResponseObservable.value!!.notifications
 
     private var updateTask = ReschedulableTask(scope, SystemClock::elapsedRealtime) {
-        update()
+        updateInternal()
     }
 
     init {
@@ -51,7 +52,13 @@ class AppConfig(scope: CoroutineScope, val api: ProtonApiRetroFit, val userData:
         updateTask.scheduleIn(0)
     }
 
-    suspend fun update() {
+    fun update() {
+        scope.launch {
+            updateInternal()
+        }
+    }
+
+    private suspend fun updateInternal() {
         val result = api.getAppConfig()
         result.valueOrNull?.let { config ->
             Storage.save(config)
