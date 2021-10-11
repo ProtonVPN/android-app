a29e50051c0e10c1b5251501af9a32c4fd8bada0
Mateusz Markowicz <mateusz.markowicz@proton.ch>
2021-10-11 12:46:14 +0200

    Convert ProtonHomeActivityTestRule to kotlin

---
+++ app/src/androidTest/java/com/protonvpn/tests/testRules/ProtonHomeActivityTestRule.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2019 Proton Technologies AG
+ *
+ * This file is part of ProtonVPN.
+ *
+ * ProtonVPN is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonVPN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.protonvpn.tests.testRules
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.rule.ActivityTestRule
+import com.protonvpn.android.ui.home.HomeActivity
+import com.protonvpn.android.vpn.ErrorType
+import com.protonvpn.android.vpn.VpnState
+import com.protonvpn.testsHelper.ServiceTestHelper
+import com.protonvpn.testsHelper.UserDataHelper
+import org.junit.runner.Description
+
+class ProtonHomeActivityTestRule : InstantTaskExecutorRule() {
+
+    private lateinit var service: ServiceTestHelper
+
+    var activityTestRule = ActivityTestRule(HomeActivity::class.java, false, false)
+    override fun starting(description: Description) {
+        super.starting(description)
+        service = ServiceTestHelper()
+        activityTestRule.launchActivity(null)
+    }
+
+    override fun finished(description: Description) {
+        super.finished(description)
+        service.enableSecureCore(false)
+        service.connectionManager.disconnect()
+        UserDataHelper().userData.logout()
+        service.deleteCreatedProfiles()
+        activityTestRule.finishActivity()
+    }
+
+    fun mockStatusOnConnect(state: VpnState) {
+        service.mockVpnBackend.stateOnConnect = state
+    }
+
+    fun mockErrorOnConnect(type: ErrorType) {
+        service.mockVpnBackend.stateOnConnect = VpnState.Error(type, null)
+    }
+
+    val activity: HomeActivity
+        get() = activityTestRule.activity
+}
