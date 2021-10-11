c2448f3b4d2a4289ef744f4d410d3f6503d03a3c
Mateusz Markowicz <mateusz.markowicz@proton.ch>
2021-10-11 13:47:08 +0200

    Convert ProtonLoginActivityTestRule to kotlin

---
+++ app/src/androidTest/java/com/protonvpn/tests/testRules/ProtonLoginActivityTestRule.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2019 Proton Technologies AG
+ *
+ * This file is part of ProtonVPN.
+ *
+ * ProtonVPN is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonVPN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.protonvpn.tests.testRules
+
+import androidx.test.rule.ActivityTestRule
+import com.protonvpn.android.models.config.UserData
+import com.protonvpn.android.ui.login.LoginActivity
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+class ProtonLoginActivityTestRule : TestWatcher() {
+
+    private val activityTestRule = ActivityTestRule(LoginActivity::class.java, false, false)
+    private lateinit var userData: UserData
+
+    override fun starting(description: Description) {
+        activityTestRule.launchActivity(null)
+        userData = activityTestRule.activity.viewModel.userData
+    }
+
+    override fun finished(description: Description) {
+        userData.logout()
+    }
+
+    fun getActivity(): LoginActivity? =
+        activityTestRule.activity
+}
