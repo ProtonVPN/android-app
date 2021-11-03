1823caedeef9fef49d59f2200e86b0acf0646483
Mateusz Markowicz <mateusz.markowicz@proton.ch>
2021-11-03 18:45:27 +0100

    Remove old login testing code (needs rewriting to core login)

--- app/src/androidTest/java/com/protonvpn/testSuites/RealConnectionSuite.kt
+++ app/src/androidTest/java/com/protonvpn/testSuites/RealConnectionSuite.kt
@@ -1,11 +1,10 @@
 package com.protonvpn.testSuites
 
-import com.protonvpn.tests.connection.RealConnectionRobotTests
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
-        RealConnectionRobotTests::class,
+        //RealConnectionRobotTests::class,
 )
 class RealConnectionSuite
