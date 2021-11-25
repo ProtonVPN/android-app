3412654b34a657626a8cd6df807e980075de46c6
Lukas Makaravičius <lukas.makaravicius@protonmail.com>
2021-11-25 17:09:08 +0200

    Use intents, instead of checking if browser is opened

--- app/src/androidTest/java/com/protonvpn/tests/account/AccountRobotTests.kt
+++ app/src/androidTest/java/com/protonvpn/tests/account/AccountRobotTests.kt
@@ -1,14 +1,25 @@
 package com.protonvpn.tests.account
 
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.isInternal
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
 import com.protonvpn.actions.AccountRobot
 import com.protonvpn.actions.HomeRobot
+import com.protonvpn.android.ui.home.HomeActivity
 import com.protonvpn.test.shared.TestUser
-import com.protonvpn.testRules.ProtonHomeActivityTestRule
 import com.protonvpn.testRules.SetUserPreferencesRule
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.hamcrest.CoreMatchers.not
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
@@ -26,17 +37,33 @@ class AccountRobotTests {
     private val accountRobot = AccountRobot()
     private val testUser = TestUser.plusUser
 
+    private val intent =
+        Intent(InstrumentationRegistry.getInstrumentation().targetContext, HomeActivity::class.java)
+
     @get:Rule
     var rules = RuleChain
-            .outerRule(HiltAndroidRule(this))
-            .around(SetUserPreferencesRule(TestUser.plusUser))
-            .around(ProtonHomeActivityTestRule())
+        .outerRule(HiltAndroidRule(this))
+        .around(SetUserPreferencesRule(TestUser.plusUser))
+
+    @Before
+    fun setUp() {
+        Intents.init()
+        ActivityScenario.launch<HomeActivity>(intent)
+        //It blocks outgoing intents and won't open browser for account tests.
+        intending(not(isInternal()))
+            .respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, null))
+    }
 
     @Test
-    fun checkIfUserNameDisplayedInAccountSection(){
+    fun checkIfUserNameDisplayedInAccountSection() {
         homeRobot.openAccountView()
         accountRobot.verify { checkIfCorrectUsernameIsDisplayed(testUser) }
         accountRobot.clickManageAccount()
-                .verify { checkIfAccountButtonOpensBrowser("com.android.chrome") }
+            .verify { checkIfAccountButtonHasCorrectUrl() }
+    }
+
+    @After
+    fun tearDown(){
+        Intents.release()
     }
 }
