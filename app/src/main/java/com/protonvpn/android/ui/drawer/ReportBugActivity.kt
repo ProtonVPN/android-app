4a6ba0df4d75167616b70fcd46ebe4a246a5557d
Marcin Simonides <marcin.simonides@proton.ch>
2021-11-30 17:05:34 +0100

    Handle back navigation in report issue screen [VPNAND-743].

--- app/src/main/java/com/protonvpn/android/ui/drawer/ReportBugActivity.kt
+++ app/src/main/java/com/protonvpn/android/ui/drawer/ReportBugActivity.kt
@@ -20,6 +20,7 @@ package com.protonvpn.android.ui.drawer
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
@@ -28,10 +29,12 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import com.protonvpn.android.R
 import com.protonvpn.android.components.BaseActivityV2
+import com.protonvpn.android.components.NetworkFrameLayout
 import com.protonvpn.android.databinding.ActivityReportBinding
 import com.protonvpn.android.utils.ViewUtils.hideKeyboard
 import com.protonvpn.android.utils.ViewUtils.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import me.proton.core.presentation.ui.view.ProtonInput
 
@@ -43,12 +46,36 @@ class ReportBugActivity : BaseActivityV2() {
     private val editReport: ProtonInput get() = binding.layoutReport.editReport
     private val editEmail: ProtonInput get() = binding.layoutReport.editEmail
 
+    private var submitJob: Job? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         initToolbarWithUpEnabled(binding.layoutAppbar.toolbar)
         addHideKeyboard(binding.coordinator)
         initUi()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) =
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+            true
+        } else {
+            super.onOptionsItemSelected(item)
+        }
+
+    override fun onBackPressed() {
+        with(binding) {
+            when(loadingContainer.state) {
+                NetworkFrameLayout.State.ERROR ->
+                    loadingContainer.switchToEmpty()
+                NetworkFrameLayout.State.LOADING -> {
+                    submitJob?.cancel()
+                    loadingContainer.switchToEmpty()
+                }
+                else -> super.onBackPressed()
+            }
+        }
     }
 
     private fun initUi() = with(binding) {
@@ -96,7 +123,8 @@ class ReportBugActivity : BaseActivityV2() {
     }
 
     private fun postReport() {
-        lifecycleScope.launch {
+        submitJob?.cancel()
+        submitJob = lifecycleScope.launch {
             viewModel.prepareAndPostReport(
                 editEmail.text.toString(),
                 editReport.text.toString(),
