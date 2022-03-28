dc3463f2585a831e8bd3af205c666da3fa4b94f3
Marcin Simonides <marcin.simonides@proton.ch>
2022-03-28 14:01:51 +0200

    Update log screen style to match the rest of the app [VPNAND-865].

--- app/src/main/java/com/protonvpn/android/ui/drawer/LogActivity.kt
+++ app/src/main/java/com/protonvpn/android/ui/drawer/LogActivity.kt
@@ -19,18 +19,70 @@
 package com.protonvpn.android.ui.drawer
 
 import android.os.Bundle
-import android.view.Window
-import com.protonvpn.android.R
-import com.protonvpn.android.components.BaseActivity
-import com.protonvpn.android.components.ContentLayout
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.protonvpn.android.components.BaseActivityV2
+import com.protonvpn.android.databinding.ActivityRecyclerWithToolbarBinding
+import com.protonvpn.android.databinding.LogItemBinding
+import com.protonvpn.android.logging.ProtonLogger
+import com.protonvpn.android.utils.AndroidUtils.setContentViewBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-@ContentLayout(R.layout.log_activity)
-class LogActivity : BaseActivity() {
+class LogActivity : BaseActivityV2() {
 
     public override fun onCreate(savedInstanceState: Bundle?) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
+        val binding = setContentViewBinding(ActivityRecyclerWithToolbarBinding::inflate)
+        initToolbarWithUpEnabled(binding.contentAppbar.toolbar)
+        setupLogDisplay(binding.recyclerItems)
+    }
+
+    private fun setupLogDisplay(recyclerView: RecyclerView) {
+        val adapter = LogAdapter()
+        val linearLayout = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = linearLayout
+        recyclerView.itemAnimator = null
+
+        var isScrolledToBottom = true
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                isScrolledToBottom = !recyclerView.canScrollVertically(1)
+            }
+        })
+
+        lifecycleScope.launch {
+            ProtonLogger.getLogLinesForDisplay().collect {
+                adapter.addLogItem(it)
+                if (isScrolledToBottom) recyclerView.scrollToPosition(adapter.itemCount - 1)
+            }
+        }
+    }
+
+    private class LogLineVH(val binding: LogItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    private class LogAdapter : RecyclerView.Adapter<LogLineVH>() {
+
+        private val log = ArrayList<String>()
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+            LogLineVH(LogItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+        override fun onBindViewHolder(holder: LogLineVH, position: Int) {
+            holder.binding.logLine.text = log[position]
+        }
+
+        override fun getItemCount() = log.size
+
+        fun addLogItem(item: String) {
+            log.add(item)
+            notifyItemInserted(log.size - 1)
+        }
     }
 }
