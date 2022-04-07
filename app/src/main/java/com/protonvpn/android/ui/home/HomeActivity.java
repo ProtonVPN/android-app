a333afd4c5f59de5b18bd7ed456460f0e433b1b9
Marcin Simonides <marcin.simonides@proton.ch>
2022-04-07 11:23:48 +0200

    Move account-related UI code to its own package.

--- app/src/main/java/com/protonvpn/android/ui/home/HomeActivity.java
+++ app/src/main/java/com/protonvpn/android/ui/home/HomeActivity.java
@@ -61,7 +61,7 @@ import com.protonvpn.android.models.profiles.Profile;
 import com.protonvpn.android.models.vpn.Server;
 import com.protonvpn.android.search.SearchResultsFragment;
 import com.protonvpn.android.search.SearchViewModel;
-import com.protonvpn.android.ui.drawer.AccountActivity;
+import com.protonvpn.android.ui.account.AccountActivity;
 import com.protonvpn.android.ui.drawer.LogActivity;
 import com.protonvpn.android.ui.drawer.bugreport.DynamicReportActivity;
 import com.protonvpn.android.ui.home.countries.CountryListFragment;
