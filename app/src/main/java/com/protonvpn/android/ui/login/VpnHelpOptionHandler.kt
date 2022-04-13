027d8d7260e34af1ddd7393d6846064634d5fbec
Marcin Simonides <marcin.simonides@proton.ch>
2022-04-13 10:50:07 +0200

    Use in-app bug report in login screen help [VPNAND-854].

---
+++ app/src/main/java/com/protonvpn/android/ui/login/VpnHelpOptionHandler.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2022. Proton AG
+ *
+ * This file is part of ProtonVPN.
+ *
+ * ProtonVPN is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonVPN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.protonvpn.android.ui.login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import com.protonvpn.android.ui.drawer.bugreport.DynamicReportActivity
+import me.proton.core.auth.presentation.DefaultHelpOptionHandler
+import javax.inject.Inject
+
+class VpnHelpOptionHandler @Inject constructor() : DefaultHelpOptionHandler() {
+
+    override fun onCustomerSupport(context: AppCompatActivity) {
+        context.startActivity(Intent(context, DynamicReportActivity::class.java))
+    }
+}
