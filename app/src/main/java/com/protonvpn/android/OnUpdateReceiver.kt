c48089ba622c2caa9ae53f0d04b059b034ba0e1b
Mateusz Markowicz <mateusz.markowicz@proton.ch>
2022-04-22 16:51:00 +0200

    Reconnect on app update

---
+++ app/src/main/java/com/protonvpn/android/OnUpdateReceiver.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2022 Proton AG
+ *
+ * This file is part of ProtonVPN.
+ *
+ * ProtonVPN is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonVPN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.protonvpn.android
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.protonvpn.android.models.vpn.ConnectionParams
+import com.protonvpn.android.utils.ServerManager
+import com.protonvpn.android.utils.Storage
+import com.protonvpn.android.vpn.VpnConnectionManager
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class OnUpdateReceiver : BroadcastReceiver() {
+
+    @Inject lateinit var serverManager: ServerManager
+    @Inject lateinit var vpnConnectionManager: VpnConnectionManager
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_MY_PACKAGE_REPLACED) {
+            Storage.load(ConnectionParams::class.java)?.profile?.let { profile ->
+                profile.wrapper.setDeliverer(serverManager)
+                vpnConnectionManager.onRestoreProcess(profile, "app update")
+            }
+        }
+    }
+}
