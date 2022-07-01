cfc00b4bcf97714c53f2b78f929c4aa55777e71b
Mateusz Markowicz <mateusz.markowicz@proton.ch>
2022-07-01 13:32:08 +0200

    Support activity result contracts with Serializable params

--- app/src/main/java/com/protonvpn/android/utils/ActivityResultUtils.kt
+++ app/src/main/java/com/protonvpn/android/utils/ActivityResultUtils.kt
@@ -24,6 +24,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Parcelable
 import androidx.activity.result.contract.ActivityResultContract
+import java.io.Serializable
 import kotlin.reflect.KClass
 
 /**
@@ -78,9 +79,32 @@ object ActivityResultUtils {
         }
     }
 
+    inline fun <reified I : Serializable, O : Serializable> createSerializableContract(
+        clazz: KClass<out Activity>
+    ): ActivityResultContract<I, O?> {
+        return object : ActivityResultContract<I, O?>() {
+            override fun createIntent(context: Context, input: I): Intent =
+                Intent(context, clazz.java).apply {
+                    putExtra(INPUT_KEY, input)
+                }
+
+            override fun parseResult(resultCode: Int, intent: Intent?): O? =
+                if (resultCode == Activity.RESULT_OK) {
+                    intent?.getSerializableExtra(OUTPUT_KEY) as O?
+                } else {
+                    null
+                }
+        }
+    }
+
     fun <O : Parcelable> setResult(activity: Activity, result: O) {
         activity.setResult(Activity.RESULT_OK, Intent().apply { putExtra(OUTPUT_KEY, result) })
     }
 
+    fun <O : Serializable> setResult(activity: Activity, result: O) {
+        activity.setResult(Activity.RESULT_OK, Intent().apply { putExtra(OUTPUT_KEY, result) })
+    }
+
     fun <I : Parcelable> getInput(intent: Intent): I? = intent.getParcelableExtra(INPUT_KEY)
+    fun <I : Serializable> getSerializableInput(intent: Intent): I? = intent.getSerializableExtra(INPUT_KEY) as I?
 }
