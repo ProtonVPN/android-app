e3f41349e2c985fe02775609c433f1989581a0b9
Marcin Simonides <marcin.simonides@proton.ch>
2022-09-19 11:23:52 +0200

    Add support for API token to Glide.

--- app/src/main/java/com/protonvpn/android/ProtonVpnGlideModule.kt
+++ app/src/main/java/com/protonvpn/android/ProtonVpnGlideModule.kt
@@ -20,7 +20,6 @@
 package com.protonvpn.android
 
 import android.content.Context
-import com.airbnb.lottie.LottieComposition
 import com.airbnb.lottie.LottieCompositionFactory
 import com.airbnb.lottie.LottieDrawable
 import com.bumptech.glide.Glide
@@ -29,9 +28,15 @@ import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.load.Options
 import com.bumptech.glide.load.ResourceDecoder
 import com.bumptech.glide.load.engine.Resource
-import com.bumptech.glide.load.resource.SimpleResource
+import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.load.model.Headers
+import com.bumptech.glide.load.model.LazyHeaders
+import com.bumptech.glide.load.model.ModelCache
+import com.bumptech.glide.load.model.ModelLoader
+import com.bumptech.glide.load.model.ModelLoaderFactory
+import com.bumptech.glide.load.model.MultiModelLoaderFactory
+import com.bumptech.glide.load.model.stream.BaseGlideUrlLoader
 import com.bumptech.glide.load.resource.drawable.DrawableResource
-import com.bumptech.glide.load.resource.transcode.ResourceTranscoder
 import com.bumptech.glide.module.AppGlideModule
 import java.io.IOException
 import java.io.InputStream
@@ -66,10 +71,45 @@ class LottieDrawableResource(drawable: LottieDrawable?) : DrawableResource<Lotti
     override fun recycle() = Unit
 }
 
+/**
+ * A URL loader supporting the test environment token.
+ * Don't use in production, it might not be fully featured.
+ */
+class BlackGlideUrlLoader : BaseGlideUrlLoader<String> {
+    constructor(concreteLoader: ModelLoader<GlideUrl, InputStream>?) : super(concreteLoader)
+    constructor(concreteLoader: ModelLoader<GlideUrl, InputStream>?, modelCache: ModelCache<String, GlideUrl>?) : super(
+        concreteLoader,
+        modelCache
+    )
+
+    override fun handles(model: String): Boolean = model.startsWith("https://")
+
+    override fun getUrl(model: String, width: Int, height: Int, options: Options?): String = model
+
+    override fun getHeaders(model: String?, width: Int, height: Int, options: Options?): Headers? =
+        if (BuildConfig.BLACK_TOKEN.isNotBlank()) {
+            LazyHeaders.Builder().addHeader("X-atlas-secret", BuildConfig.BLACK_TOKEN).build()
+        } else {
+            super.getHeaders(model, width, height, options)
+        }
+
+    class Factory : ModelLoaderFactory<String, InputStream> {
+        override fun build(multiFactory: MultiModelLoaderFactory): ModelLoader<String, InputStream> =
+            BlackGlideUrlLoader(multiFactory.build(GlideUrl::class.java, InputStream::class.java))
+
+        override fun teardown() = Unit
+    }
+}
+
 @GlideModule
 class ProtonVpnGlideModule : AppGlideModule() {
+
     override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
         registry
             .append(InputStream::class.java, LottieDrawable::class.java, StreamLottieDecoder())
+
+        if (BuildConfig.BLACK_TOKEN.isNotBlank()) {
+            registry.prepend(String::class.java, InputStream::class.java, BlackGlideUrlLoader.Factory())
+        }
     }
 }
