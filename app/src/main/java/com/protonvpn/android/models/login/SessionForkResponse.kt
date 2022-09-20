ea3f5e2d7c260ea6f6f559833e98ae66b18459c4
Marcin Simonides <marcin.simonides@proton.ch>
2022-09-20 11:25:08 +0200

    Add use case for auto login for web.
    It's not used anywhere yet.

---
+++ app/src/main/java/com/protonvpn/android/models/login/SessionForkResponse.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2022. Proton AG
+ *
+ * This file is part of ProtonVPN.
+ *
+ * ProtonVPN is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonVPN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.protonvpn.android.models.login
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+class SessionForkResponse(
+    @SerialName("Selector") val selector: String
+)
