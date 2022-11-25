ca35223e917be42adefc96b7826ae4c4fd46500b
Neil Marietta <neil.marietta@protonmail.com>
2022-11-25 16:34:38 +0100

    Replaced Git Submodules by Include Core Build Gradle Plugin.

---
+++ settings.gradle.kts
@@ -0,0 +1,33 @@
+
+rootProject.name = "ProtonVPN"
+
+plugins {
+    id("me.proton.core.gradle-plugins.include-core-build") version "1.1.1"
+}
+
+includeCoreBuild {
+    // By default, target :
+    // On CI: "https://$gitlab_host/proton/mobile/android/proton-libs.git".
+    // else: "https://github.com/ProtonMail/protoncore_android.git".
+
+    // To force using full Core sources from a specific branch.
+    // branch.set("fix/my_needed_fix")
+
+    // To force using full Core sources from a specific commit.
+    // commit.set("213e46d307195abbaa063dc03ca40e2cda9a5a6b")
+}
+
+include(":app")
+include(":detekt-gitlab-output-plugin")
+include(":openvpn")
+include(":shared-test-code")
+
+
+buildCache {
+    local {
+        if (System.getenv("CI") == "true") {
+            directory = file("build/gradle-build-cache")
+        }
+        removeUnusedEntriesAfterDays = 3
+    }
+}
