873349d5181bc43f527c03225abc971154e20b7f
Algirdas Pundzius <algirdas.pundzius@protonmail.com>
2023-02-06 16:32:07 +0200

    Update assign connections dialog [VPNAND-1131]

--- app/src/main/java/com/protonvpn/android/ui/login/AssignVpnConnectionActivity.kt
+++ app/src/main/java/com/protonvpn/android/ui/login/AssignVpnConnectionActivity.kt
@@ -19,24 +19,39 @@
 package com.protonvpn.android.ui.login
 
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import com.protonvpn.android.R
 import com.protonvpn.android.components.BaseActivityV2
 import com.protonvpn.android.databinding.ActivityAssignVpnConnectionBinding
 import com.protonvpn.android.utils.Constants.URL_SUPPORT_ASSIGN_VPN_CONNECTION
 import com.protonvpn.android.utils.openUrl
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import me.proton.core.presentation.utils.onClick
 import me.proton.core.presentation.utils.viewBinding
+import me.proton.core.user.domain.entity.Role
 
 @AndroidEntryPoint
 class AssignVpnConnectionActivity : BaseActivityV2() {
 
     private val binding by viewBinding(ActivityAssignVpnConnectionBinding::inflate)
+    private val viewModel by viewModels<AssignVpnConnectionViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
         with(binding) {
+            lifecycleScope.launch {
+                val role = viewModel.getUserRole()
+                val isAdmin = role == Role.OrganizationAdmin
+                buttonAssignVpnConnections.isVisible = isAdmin
+                textDescriptionAssignConnections.text =
+                    getString(if (isAdmin) R.string.connectionAllocationHelpDescription1 else R.string.connectionAllocationHelpDescription1AskAdmin)
+                textSubDescriptionAssignConnections.isVisible = isAdmin
+
+            }
             buttonAssignVpnConnections.onClick {
                 openUrl(URL_SUPPORT_ASSIGN_VPN_CONNECTION)
             }
