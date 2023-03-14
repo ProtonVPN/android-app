10646b74dfcc362b01c5c3fdd17ee93bc1f3a6ba
Marcin Simonides <marcin.simonides@proton.ch>
2023-03-14 09:52:49 +0100

    Logout Amazon tablet users who previously had TV UI [VPNAND-1204].

--- app/src/main/java/com/protonvpn/android/utils/Constants.kt
+++ app/src/main/java/com/protonvpn/android/utils/Constants.kt
@@ -49,6 +49,7 @@ object Constants {
     const val VPN_INFO_REFRESH_INTERVAL_MINUTES = 3
     const val WIREGUARD_TUNNEL_NAME = "ProtonTunnel"
     const val SECONDARY_PROCESS_TAG = "SecondaryProcess"
+    const val FLAVOR_AMAZON = "amazon"
 
     val CLIENT_ID: String
     val VPN_USERNAME_PRODUCT_SUFFIX: String
