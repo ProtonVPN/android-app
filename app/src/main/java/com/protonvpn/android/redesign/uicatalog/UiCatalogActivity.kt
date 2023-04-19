4e3a2ab85c9b08026b97e0481f7e411cfac3b1e1
Marcin Simonides <marcin.simonides@proton.ch>
2023-04-19 09:42:03 +0200

    Make UI catalog available in release builds.

--- app/src/main/java/com/protonvpn/android/redesign/uicatalog/UiCatalogActivity.kt
+++ app/src/main/java/com/protonvpn/android/redesign/uicatalog/UiCatalogActivity.kt
@@ -17,7 +17,7 @@
  * along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package com.protonvpn.android.uicatalog
+package com.protonvpn.android.redesign.uicatalog
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
