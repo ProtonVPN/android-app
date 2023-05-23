d18360f94fe00a911a4135c271026db177b3dcf2
Marcin Simonides <marcin.simonides@proton.ch>
2023-05-23 12:09:14 +0200

    Revert "Use VpnService.protect instead of split tunneling for UDP socket"
    This reverts commit 9af7d76137796d07a025f5d9e4ea15fe3a390fc0.
    
    The change causes a crash in certain conditions, see VPNAND-1279.

--- app/src/main/java/com/protonvpn/android/vpn/wireguard/WireguardBackend.kt
+++ app/src/main/java/com/protonvpn/android/vpn/wireguard/WireguardBackend.kt
@@ -128,7 +128,8 @@ class WireguardBackend @Inject constructor(
             val config = wireguardParams.getTunnelConfig(
                 context, userData, currentUser.sessionId(), certificateRepository
             )
-            val transmissionStr = wireguardParams.transmission.toString().lowercase()
+            val transmission = wireguardParams.protocolSelection?.transmission ?: TransmissionProtocol.UDP
+            val transmissionStr = transmission.toString().lowercase()
             withContext(wireGuardIo) {
                 try {
                     backend.setState(testTunnel, Tunnel.State.UP, config, transmissionStr)
