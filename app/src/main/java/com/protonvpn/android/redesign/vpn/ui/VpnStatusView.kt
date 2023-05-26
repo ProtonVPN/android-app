35d7db1391729372fa99fac4ec34ef1601bdeb96
Algirdas Pundzius <algirdas.pundzius@protonmail.com>
2023-05-26 15:08:50 +0300

    Split code into multiple composables add animated gradient and show connected animation only after connecting state has happened

--- app/src/main/java/com/protonvpn/android/redesign/vpn/ui/VpnStatusView.kt
+++ app/src/main/java/com/protonvpn/android/redesign/vpn/ui/VpnStatusView.kt
@@ -20,10 +20,15 @@
 package com.protonvpn.android.redesign.vpn.ui
 
 import android.text.BidiFormatter
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
@@ -37,12 +42,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -81,98 +89,186 @@ data class LocationText(
 @Composable
 fun VpnStatusView(
     stateFlow: StateFlow<VpnStatusViewState>,
-    modifier: Modifier = Modifier.fillMaxWidth()
+    modifier: Modifier = Modifier
 ) {
     val statusState = stateFlow.collectAsStateWithLifecycle()
+    val targetColor = when (statusState.value) {
+        is VpnStatusViewState.Connected -> ProtonTheme.colors.vpnGreen
+        is VpnStatusViewState.Connecting -> ProtonTheme.colors.shade100
+        is VpnStatusViewState.Disabled -> ProtonTheme.colors.notificationError
+    }
+
+    val gradientColor = animateColorAsState(
+        targetValue = targetColor,
+        animationSpec = tween(durationMillis = 500)
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(
+                        gradientColor.value.copy(alpha = 0.5F),
+                        gradientColor.value.copy(alpha = 0.0F)
+                    ),
+                    startY = 0f,
+                    endY = Float.POSITIVE_INFINITY
+                )
+            )
+    ) {
+        StatusView(
+            stateFlow = stateFlow,
+            modifier.padding(16.dp)
+        )
+    }
+}
+
+@Composable
+private fun StatusView(
+    stateFlow: StateFlow<VpnStatusViewState>,
+    modifier: Modifier = Modifier
+) {
+    val statusState = stateFlow.collectAsStateWithLifecycle()
+    val wasConnecting = remember { mutableStateOf(false) }
 
     Box(modifier, contentAlignment = Alignment.Center) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            val shouldAnimate =
+                wasConnecting.value && statusState.value is VpnStatusViewState.Connected
             when (val state = statusState.value) {
                 is VpnStatusViewState.Connected -> {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier.padding(8.dp)
-                    ) {
-                        Icon(
-                            painter = painterResource(
-                                id = if (state.isSecureCoreServer) R.drawable.ic_proton_locks else R.drawable.ic_proton_lock
-                            ),
-                            tint = ProtonTheme.colors.vpnGreen,
-                            contentDescription = null,
-                            modifier = Modifier.padding(end = 8.dp)
-                        )
-                        Text(
-                            text = stringResource(R.string.vpn_status_connected),
-                            style = ProtonTheme.typography.defaultStrongNorm,
-                            color = ProtonTheme.colors.vpnGreen,
-                        )
-                    }
-                    Surface(
-                        color = ProtonTheme.colors.backgroundDeep,
-                        shape = ProtonTheme.shapes.medium,
-                        modifier = Modifier.padding(8.dp)
-                    ) {
-                        BandwidthStatsRow(false, state.netShieldStats)
-                    }
+                    VpnConnectedView(state, shouldAnimate)
                 }
 
                 is VpnStatusViewState.Connecting -> {
-                    CircularProgressIndicator(
-                        color = ProtonTheme.colors.iconNorm,
-                        strokeWidth = 2.dp,
-                        modifier = Modifier
-                            .padding(8.dp)
-                            .size(24.dp)
-                    )
-                    Text(
-                        text = stringResource(R.string.vpn_status_connecting),
-                        style = ProtonTheme.typography.defaultStrongNorm,
-                        color = ProtonTheme.colors.textNorm,
-                        modifier = Modifier.padding(8.dp)
-                    )
-                    Surface(
-                        color = ProtonTheme.colors.backgroundDeep,
-                        shape = ProtonTheme.shapes.medium
-                    ) {
-                        AnimateText(
-                            targetText = state.locationText?.run {
-                                stringResource(R.string.vpn_status_disabled_location, BidiFormatter.getInstance().unicodeWrap(country), ip)
-                            } ?: "",
-                            targetCharacter = '*',
-                            modifier = Modifier.padding(start = 8.dp, end = 8.dp),
-                        )
-                    }
+                    VpnConnectingView(state)
+                    wasConnecting.value = true
                 }
 
                 is VpnStatusViewState.Disabled -> {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_proton_lock_open),
-                        contentDescription = null,
-                        tint = ProtonTheme.colors.notificationError,
-                        modifier = Modifier.padding(8.dp)
-                    )
-                    Text(
-                        text = stringResource(R.string.vpn_status_disabled),
-                        style = ProtonTheme.typography.defaultStrongNorm,
-                        modifier = Modifier.padding(8.dp)
-                    )
-                    Surface(
-                        color = ProtonTheme.colors.backgroundDeep,
-                        shape = ProtonTheme.shapes.medium,
-                    ) {
-                        Text(
-                            text = state.locationText?.run {
-                                stringResource(R.string.vpn_status_disabled_location, BidiFormatter.getInstance().unicodeWrap(country), ip)
-                            } ?: stringResource(R.string.stateFragmentUnknownIp),
-                            style = ProtonTheme.typography.defaultWeak,
-                            modifier = Modifier.padding(horizontal = 8.dp)
-                        )
-                    }
+                    VpnDisabledView(state)
                 }
             }
         }
     }
 }
+
+@Composable
+private fun VpnConnectedView(
+    state: VpnStatusViewState.Connected,
+    shouldAnimate: Boolean,
+) {
+    val offsetY = remember { Animatable(if (shouldAnimate) 1f else 0f) }
+    val alpha = remember { Animatable(if (shouldAnimate) 0f else 1f) }
+
+    if (shouldAnimate) {
+        LaunchedEffect(Unit) {
+            offsetY.animateTo(0f, animationSpec = tween(500))
+        }
+        LaunchedEffect(Unit) {
+            alpha.animateTo(1f, animationSpec = tween(500))
+        }
+    }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(8.dp)
+            .alpha(alpha.value)
+    ) {
+        Icon(
+            painter = painterResource(
+                id = if (state.isSecureCoreServer) R.drawable.ic_proton_locks else R.drawable.ic_proton_lock
+            ),
+            tint = ProtonTheme.colors.vpnGreen,
+            contentDescription = null,
+            modifier = Modifier.padding(end = 8.dp)
+        )
+        Text(
+            text = stringResource(R.string.vpn_status_connected),
+            style = ProtonTheme.typography.defaultStrongNorm,
+            color = ProtonTheme.colors.vpnGreen,
+
+            )
+    }
+
+    Surface(
+        color = ProtonTheme.colors.backgroundDeep,
+        shape = ProtonTheme.shapes.medium,
+        modifier = Modifier
+            .offset { IntOffset(x = 0, y = (offsetY.value * 50).toInt()) }
+            .alpha(alpha.value)
+            .padding(top = 8.dp)
+    ) {
+        BandwidthStatsRow(false, state.netShieldStats)
+    }
+}
+
+@Composable
+private fun VpnConnectingView(state: VpnStatusViewState.Connecting) {
+    CircularProgressIndicator(
+        color = ProtonTheme.colors.iconNorm,
+        strokeWidth = 2.dp,
+        modifier = Modifier
+            .padding(8.dp)
+            .size(24.dp)
+    )
+    Text(
+        text = stringResource(R.string.vpn_status_connecting),
+        style = ProtonTheme.typography.defaultStrongNorm,
+        color = ProtonTheme.colors.textNorm,
+        modifier = Modifier.padding(8.dp)
+    )
+    Surface(
+        color = ProtonTheme.colors.backgroundDeep,
+        shape = ProtonTheme.shapes.medium
+    ) {
+        AnimateText(
+            targetText = state.locationText?.run {
+                stringResource(
+                    R.string.vpn_status_disabled_location,
+                    BidiFormatter.getInstance().unicodeWrap(country),
+                    ip
+                )
+            } ?: "",
+            targetCharacter = '*',
+            modifier = Modifier.padding(start = 8.dp, end = 8.dp),
+        )
+    }
+}
+
+@Composable
+private fun VpnDisabledView(state: VpnStatusViewState.Disabled) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_proton_lock_open),
+        contentDescription = null,
+        tint = ProtonTheme.colors.notificationError,
+        modifier = Modifier.padding(8.dp)
+    )
+    Text(
+        text = stringResource(R.string.vpn_status_disabled),
+        style = ProtonTheme.typography.defaultStrongNorm,
+        modifier = Modifier.padding(8.dp)
+    )
+    Surface(
+        color = ProtonTheme.colors.backgroundDeep,
+        shape = ProtonTheme.shapes.medium,
+    ) {
+        Text(
+            text = state.locationText?.run {
+                stringResource(
+                    R.string.vpn_status_disabled_location,
+                    BidiFormatter.getInstance().unicodeWrap(country),
+                    ip
+                )
+            } ?: stringResource(R.string.stateFragmentUnknownIp),
+            style = ProtonTheme.typography.defaultWeak,
+            modifier = Modifier.padding(horizontal = 8.dp)
+        )
+    }
+}
+
 
 @Composable
 private fun AnimateText(
@@ -230,7 +326,14 @@ private fun AnimateText(
 @Composable
 private fun PreviewVpnDisabledState() {
     VpnStatusView(
-        stateFlow = MutableStateFlow(VpnStatusViewState.Disabled(LocationText("Europe", "192.1.1.1.1"))),
+        stateFlow = MutableStateFlow(
+            VpnStatusViewState.Disabled(
+                LocationText(
+                    "Europe",
+                    "192.1.1.1.1"
+                )
+            )
+        ),
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp)
