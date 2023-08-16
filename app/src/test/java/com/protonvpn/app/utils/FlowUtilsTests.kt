c9281d58d6eddfe7de518d96d59dbbcf215a47bf
Marcin Simonides <marcin.simonides@proton.ch>
2023-08-16 11:30:57 +0200

    Fix the new StateFlow.mapState operator.

--- app/src/test/java/com/protonvpn/app/utils/FlowUtilsTests.kt
+++ app/src/test/java/com/protonvpn/app/utils/FlowUtilsTests.kt
@@ -19,11 +19,16 @@
 
 package com.protonvpn.app.utils
 
+import com.protonvpn.android.utils.mapState
 import com.protonvpn.android.utils.withPrevious
+import com.protonvpn.test.shared.runWhileCollecting
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -47,5 +52,22 @@ class FlowUtilsTests {
     fun `withPrevious works with nullable values`() = runTest {
         val result = flowOf(null, null, null, 1).withPrevious().toList()
         assertEquals(listOf(Pair(null, null), Pair(null, null), Pair(null, 1)), result)
+    }
+
+    @Test
+    fun `mapState maps every value`() = runTest {
+        val source = MutableStateFlow(1)
+        val mapped = source.mapState { it.toString() }
+        source.map {  }
+
+        assertEquals("1", mapped.value)
+        val updates = runWhileCollecting(mapped) {
+            runCurrent()
+            source.value = 2
+            runCurrent()
+            source.value = 3
+            runCurrent()
+        }
+        assertEquals(listOf("1", "2", "3"), updates)
     }
 }
