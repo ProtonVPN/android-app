ebdb18330a8e29e3e47b490dea69ef1ebc4d052d
Neil Marietta <neil.marietta@protonmail.com>
2023-09-08 17:58:24 +0200

    refactor!: Removed `Logger.log` function.

--- app/src/main/java/com/protonvpn/android/utils/VpnCoreLogger.kt
+++ app/src/main/java/com/protonvpn/android/utils/VpnCoreLogger.kt
@@ -26,94 +26,105 @@ import com.protonvpn.android.logging.LogCategory
 import com.protonvpn.android.logging.LogLevel
 import com.protonvpn.android.logging.ProtonLogger
 import me.proton.core.accountmanager.domain.LogTag as AccountLogTag
-import me.proton.core.humanverification.presentation.LogTag
+import me.proton.core.humanverification.presentation.LogTag as HvLogTag
 import me.proton.core.util.android.sentry.TimberLogger
 import me.proton.core.crypto.common.keystore.LogTag as KeystoreLogTag
 import me.proton.core.network.domain.LogTag as NetworkLogTag
 import me.proton.core.util.kotlin.Logger
-import me.proton.core.util.kotlin.LoggerLogTag
 
 // Core logs full response body in debug, truncate it.
 private const val MAX_DEBUG_MSG_LENGTH = 500
 
 class VpnCoreLogger : Logger by TimberLogger {
 
-    override fun log(tag: LoggerLogTag, message: String) {
-        TimberLogger.log(tag, message)
-        when (tag) {
-            AccountLogTag.SESSION_REFRESH ->
-                ProtonLogger.logCustom(LogCategory.API, message)
-            AccountLogTag.SESSION_REQUEST ->
-                ProtonLogger.logCustom(LogCategory.API, message)
-            AccountLogTag.SESSION_FORCE_LOGOUT ->
-                ProtonLogger.logCustom(LogCategory.API, message)
-            NetworkLogTag.SERVER_TIME_PARSE_ERROR ->
-                ProtonLogger.logCustom(LogLevel.ERROR, LogCategory.API, message)
-            NetworkLogTag.API_REQUEST ->
-                ProtonLogger.log(ApiLogRequest, message)
-            NetworkLogTag.API_RESPONSE ->
-                ProtonLogger.log(ApiLogResponse, message)
-            NetworkLogTag.API_ERROR ->
-                ProtonLogger.log(ApiLogError, message)
-            LogTag.HV_REQUEST_ERROR ->
-                ProtonLogger.logCustom(LogLevel.ERROR, LogCategory.API, message)
-            else -> {
-                DebugUtils.debugAssert("Unknown log tag. Update this mapping.") { true }
-                ProtonLogger.logCustom(LogCategory.APP, "[${tag.name}] $message")
-            }
-        }
+    override fun e(tag: String, message: String) {
+        TimberLogger.e(tag, message)
+        forwardToProtonLogger(tag, LogLevel.ERROR, message)
     }
 
     override fun e(tag: String, e: Throwable) {
-        e(tag, e, "")
+        TimberLogger.e(tag, e)
+        forwardToProtonLogger(tag, LogLevel.ERROR, messageWithError("no message", e))
     }
 
     override fun e(tag: String, e: Throwable, message: String) {
         TimberLogger.e(tag, e, message)
-        ProtonLogger.logCustom(LogLevel.ERROR, categoryForTag(tag), messageWithError(message, e))
+        forwardToProtonLogger(tag, LogLevel.ERROR, messageWithError(message, e))
     }
 
     override fun i(tag: String, message: String) {
         TimberLogger.i(tag, message)
-        ProtonLogger.logCustom(LogLevel.INFO, categoryForTag(tag), message)
+        forwardToProtonLogger(tag, LogLevel.INFO, message)
     }
 
     override fun i(tag: String, e: Throwable, message: String) {
         TimberLogger.i(tag, e, message)
-        ProtonLogger.logCustom(LogLevel.INFO, categoryForTag(tag), messageWithError(message, e))
+        forwardToProtonLogger(tag, LogLevel.INFO, messageWithError(message, e))
+    }
+
+    override fun w(tag: String, message: String) {
+        TimberLogger.w(tag, message)
+        forwardToProtonLogger(tag, LogLevel.WARN, message)
+    }
+
+    override fun w(tag: String, e: Throwable) {
+        TimberLogger.w(tag, e)
+        forwardToProtonLogger(tag, LogLevel.WARN, messageWithError("no message", e))
+    }
+
+    override fun w(tag: String, e: Throwable, message: String) {
+        TimberLogger.w(tag, e, message)
+        forwardToProtonLogger(tag, LogLevel.WARN, messageWithError(message, e))
     }
 
     override fun d(tag: String, message: String) {
         TimberLogger.d(tag, message)
-        ProtonLogger.logCustom(LogLevel.DEBUG, categoryForTag(tag), message.take(MAX_DEBUG_MSG_LENGTH))
+        forwardToProtonLogger(tag, LogLevel.DEBUG, message.take(MAX_DEBUG_MSG_LENGTH))
     }
 
     override fun d(tag: String, e: Throwable, message: String) {
         TimberLogger.d(tag, e, message)
-        ProtonLogger.logCustom(LogLevel.DEBUG, categoryForTag(tag), messageWithError(message, e))
+        forwardToProtonLogger(tag, LogLevel.DEBUG, messageWithError(message, e))
     }
 
     override fun v(tag: String, message: String) {
         TimberLogger.v(tag, message)
-        ProtonLogger.logCustom(LogLevel.TRACE, categoryForTag(tag), message)
+        forwardToProtonLogger(tag, LogLevel.TRACE, message)
     }
 
     override fun v(tag: String, e: Throwable, message: String) {
         TimberLogger.v(tag, e, message)
-        ProtonLogger.logCustom(LogLevel.TRACE, categoryForTag(tag), messageWithError(message, e))
+        forwardToProtonLogger(tag, LogLevel.TRACE, messageWithError(message, e))
     }
 
     private fun messageWithError(message: String, e: Throwable) =
         "$message\n${e.stackTraceToString()}"
 
     private fun categoryForTag(tag: String) = when (tag) {
-        KeystoreLogTag.KEYSTORE_INIT, KeystoreLogTag.KEYSTORE_ENCRYPT, KeystoreLogTag.KEYSTORE_DECRYPT ->
-            LogCategory.SECURE_STORE
-        NetworkLogTag.DEFAULT ->
-            LogCategory.API
+        AccountLogTag.SESSION_REFRESH -> LogCategory.API
+        AccountLogTag.SESSION_REQUEST -> LogCategory.API
+        AccountLogTag.SESSION_FORCE_LOGOUT -> LogCategory.API
+        NetworkLogTag.SERVER_TIME_PARSE_ERROR -> LogCategory.API
+        NetworkLogTag.API_REQUEST -> LogCategory.API
+        NetworkLogTag.API_RESPONSE -> LogCategory.API
+        NetworkLogTag.API_ERROR -> LogCategory.API
+        NetworkLogTag.DEFAULT -> LogCategory.API
+        HvLogTag.HV_REQUEST_ERROR -> LogCategory.API
+        KeystoreLogTag.KEYSTORE_INIT -> LogCategory.SECURE_STORE
+        KeystoreLogTag.KEYSTORE_ENCRYPT -> LogCategory.SECURE_STORE
+        KeystoreLogTag.KEYSTORE_DECRYPT -> LogCategory.SECURE_STORE
         else -> {
             DebugUtils.debugAssert("Unknown log tag. Update this mapping.") { true }
             LogCategory.APP
+        }
+    }
+
+    private fun forwardToProtonLogger(tag: String, sourceLevel: LogLevel, message: String) {
+        when (tag) {
+            NetworkLogTag.API_REQUEST -> ProtonLogger.log(ApiLogRequest, message)
+            NetworkLogTag.API_RESPONSE -> ProtonLogger.log(ApiLogResponse, message)
+            NetworkLogTag.API_ERROR -> ProtonLogger.log(ApiLogError, message)
+            else -> ProtonLogger.logCustom(sourceLevel, categoryForTag(tag), message)
         }
     }
 }
