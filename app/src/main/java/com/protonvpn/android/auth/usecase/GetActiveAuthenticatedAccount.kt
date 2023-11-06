8711222947b625f8104a66951e5631053485f460
Mateusz Markowicz <mateusz.markowicz@proton.ch>
2023-11-06 18:36:18 +0100

    Refresh app config together with core feature flags

---
+++ app/src/main/java/com/protonvpn/android/auth/usecase/GetActiveAuthenticatedAccount.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2023. Proton AG
+ *
+ * This file is part of ProtonVPN.
+ *
+ * ProtonVPN is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonVPN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.protonvpn.android.auth.usecase
+
+import kotlinx.coroutines.flow.first
+import me.proton.core.account.domain.entity.Account
+import me.proton.core.account.domain.entity.AccountState
+import me.proton.core.account.domain.entity.SessionState
+import me.proton.core.accountmanager.domain.AccountManager
+import me.proton.core.accountmanager.domain.getPrimaryAccount
+import javax.inject.Inject
+import javax.inject.Singleton
+
+// Returns account that is logged in or in the process of.
+@Singleton
+class GetActiveAuthenticatedAccount @Inject constructor(
+    private val accountManager: AccountManager
+) {
+    suspend operator fun invoke() : Account? =
+        accountManager.getPrimaryAccount().first() ?:
+        accountManager.getAccounts().first().firstOrNull {
+            it.sessionState == SessionState.Authenticated
+                && it.state != AccountState.Disabled
+                && it.state != AccountState.Removed
+        }
+}
