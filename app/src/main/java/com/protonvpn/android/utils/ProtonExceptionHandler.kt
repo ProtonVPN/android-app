4e2e5e2d5977ba6c3e450ed5cb774e8e5a7bf4d5
Mateusz Markowicz <mateusz.markowicz@proton.ch>
2023-11-10 13:55:16 +0100

    Put a limit on stacktrace size we're logging

--- app/src/main/java/com/protonvpn/android/utils/ProtonExceptionHandler.kt
+++ app/src/main/java/com/protonvpn/android/utils/ProtonExceptionHandler.kt
@@ -20,15 +20,11 @@ package com.protonvpn.android.utils
 
 import com.protonvpn.android.logging.AppCrash
 import com.protonvpn.android.logging.ProtonLogger
-import java.io.PrintWriter
-import java.io.StringWriter
 
 class ProtonExceptionHandler(val innerHandler: Thread.UncaughtExceptionHandler?) : Thread.UncaughtExceptionHandler {
 
     override fun uncaughtException(t: Thread, e: Throwable) {
-        val writer = StringWriter()
-        e.printStackTrace(PrintWriter(writer))
-        ProtonLogger.logBlocking(AppCrash, writer.toString())
+        ProtonLogger.logBlocking(AppCrash, e.stacktraceMessage().toString())
         innerHandler?.uncaughtException(t, e)
     }
 }
