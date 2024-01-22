6624c417b115af8b197b343f9e90258281b48abf
Mateusz Markowicz <mateusz.markowicz@proton.ch>
2024-01-22 16:38:41 +0100

    Rename old-map coordinates to more explicit

--- app/src/main/java/com/protonvpn/android/tv/main/TvMainViewModel.kt
+++ app/src/main/java/com/protonvpn/android/tv/main/TvMainViewModel.kt
@@ -136,7 +136,7 @@ class TvMainViewModel @Inject constructor(
 
     fun getCountryCardMap(): Map<CountryTools.Continent?, List<CountryCard>> {
         return serverManager.getVpnCountries().groupBy({
-            val continent = CountryTools.locationMap[it.flag]?.continent
+            val continent = CountryTools.oldMapLocations[it.flag]?.continent
             DebugUtils.debugAssert("Unknown location for ${it.flag}") { continent != null || it.flag == "XX" }
             continent
         }, { country ->
