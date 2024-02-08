42f297193749734f8157838cb11c4f8483dc9423
Mateusz Armatys <mateusz.armatys@proton.ch>
2024-02-08 15:18:06 +0100

    test(app): Add UI tests for logging in with a credential-less (guest) account.

--- app/src/androidTest/java/com/protonvpn/testSuites/MobileBlackSuite.kt
+++ app/src/androidTest/java/com/protonvpn/testSuites/MobileBlackSuite.kt
@@ -1,6 +1,7 @@
 package com.protonvpn.testSuites
 
 import com.protonvpn.tests.login.LoginTests
+import com.protonvpn.tests.signin.SignInGuestTests
 import com.protonvpn.tests.signin.SignInTests
 import com.protonvpn.tests.signup.SignupTests
 import org.junit.runner.RunWith
@@ -11,6 +12,7 @@ import org.junit.runners.Suite
     LoginTests::class,
     SignInTests::class,
     SignupTests::class,
+    SignInGuestTests::class,
     //BugReportTests::class, TODO Implement this when bug report is implemented on redesign.
     //TokenExpirationTests::class, TODO Implement this when there is a way to trigger API call from UI.
 )
