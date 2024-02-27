b53e81d03b7b566d6bef333691948fb2b5997bbf
Marcin Simonides <marcin.simonides@proton.ch>
2024-02-27 12:22:23 +0100

    Fix memory leak [VPNAND-1654].
    Don't keep reference to the last Activity in withPrevious() operator.

--- app/src/main/java/com/protonvpn/android/utils/FlowUtils.kt
+++ app/src/main/java/com/protonvpn/android/utils/FlowUtils.kt
@@ -24,7 +24,6 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
