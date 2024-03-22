3b413e692a62bcdd55bfd3d88f92819735d531fc
Marcin Simonides <marcin.simonides@proton.ch>
2024-03-22 15:17:56 +0100

    Set max size for settings screens [VPNAND-1698].

--- app/src/main/java/com/protonvpn/android/redesign/main_screen/ui/nav/MainNav.kt
+++ app/src/main/java/com/protonvpn/android/redesign/main_screen/ui/nav/MainNav.kt
@@ -26,7 +26,6 @@ import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
