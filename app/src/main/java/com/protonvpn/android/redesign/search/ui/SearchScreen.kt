15342044e8017dc5a98570590145d4333a264119
Mateusz Markowicz <mateusz.markowicz@proton.ch>
2024-04-26 13:25:56 +0200

    Provide navigation bar padding for search screen

--- app/src/main/java/com/protonvpn/android/redesign/search/ui/SearchScreen.kt
+++ app/src/main/java/com/protonvpn/android/redesign/search/ui/SearchScreen.kt
@@ -33,6 +33,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
@@ -78,6 +79,7 @@ fun SearchRoute(
         modifier = Modifier
             .fillMaxSize()
             .background(ProtonTheme.colors.backgroundNorm)
+            .navigationBarsPadding()
             .imePadding()
     ) {
         val viewModel: SearchViewModel = hiltViewModel()
