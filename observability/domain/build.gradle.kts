eb63a520f691383be15d609c0b7ab51fbb7c91bb
Marcin Simonides <marcin.simonides@proton.ch>
2024-05-10 16:33:43 +0200

    Add observability metrics for CertificateStorage [VPNAND-1767].

---
+++ observability/domain/build.gradle.kts
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2024. Proton AG
+ *
+ * This file is part of ProtonVPN.
+ *
+ * ProtonVPN is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonVPN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.serialization")
+}
+
+dependencies {
+    val core_version: String by project
+    val kotlinx_serialization_json_version: String by project
+
+    implementation("me.proton.core:observability-domain:$core_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinx_serialization_json_version")
+}
