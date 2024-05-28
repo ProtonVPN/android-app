4a6904a4ac9e4f11b06212b287f3ae038ca5fa93
Mateusz Markowicz <mateusz.markowicz@proton.ch>
2024-05-28 16:22:59 +0200

    Remove feature flag UnrestrictedChangeServerOnLongConnect [VPNAND-1768]

--- app/src/main/java/com/protonvpn/android/redesign/vpn/ChangeServerManager.kt
+++ app/src/main/java/com/protonvpn/android/redesign/vpn/ChangeServerManager.kt
@@ -29,56 +29,22 @@ import com.protonvpn.android.vpn.VpnConnectionManager
 import com.protonvpn.android.vpn.VpnState
 import com.protonvpn.android.vpn.VpnStatusProviderUI
 import com.protonvpn.android.vpn.VpnUiDelegate
-import dagger.Reusable
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.transformLatest
 import kotlinx.coroutines.launch
-import me.proton.core.domain.entity.UserId
-import me.proton.core.featureflag.domain.entity.FeatureId
-import me.proton.core.featureflag.domain.repository.FeatureFlagRepository
-import me.proton.core.network.domain.ApiException
 import javax.inject.Inject
 import javax.inject.Singleton
 
 private const val ALLOW_CHANGE_SERVER_CONNECT_DELAY_MS = 6_000L
-
-@Reusable
-class UnrestrictedChangeServerOnLongConnectEnabled @Inject constructor(
-    currentUser: CurrentUser,
-    private val featureFlagRepository: FeatureFlagRepository
-) {
-    @OptIn(ExperimentalCoroutinesApi::class)
-    val isEnabled = currentUser.userFlow.flatMapLatest { user ->
-        if (user == null) flowOf(false)
-        else getFeatureFlagFlow(user.userId, FEATURE_ID)
-    }
-
-    private fun getFeatureFlagFlow(userId: UserId, featureId: FeatureId) =
-        featureFlagRepository
-            .observe(userId, featureId, refresh = false)
-            .map { flag -> flag?.value ?: false }
-            .catch { e ->
-                if (e !is ApiException) throw e
-                emit(false)
-            }
-
-    companion object {
-        private val FEATURE_ID = FeatureId("UnrestrictedChangeServerOnLongConnect")
-    }
-}
 
 @Singleton
 class ChangeServerManager @Inject constructor(
@@ -89,7 +55,6 @@ class ChangeServerManager @Inject constructor(
     private val serverManager: ServerManager2,
     private val changeServerPrefs: ChangeServerPrefs,
     private val currentUser: CurrentUser,
-    isUnrestrictedChangeServerEnabled: UnrestrictedChangeServerOnLongConnectEnabled,
     @WallClock private val wallClock: () -> Long
 ) {
 
@@ -97,13 +62,7 @@ class ChangeServerManager @Inject constructor(
     val isChangingServer: StateFlow<Boolean> get() = changeInProgress
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    val hasTroubleConnecting = isUnrestrictedChangeServerEnabled.isEnabled.flatMapLatest { isEnabled ->
-        if (isEnabled) hasTroubleConnectingFlow
-        else flowOf(false)
-    }.stateIn(mainScope, SharingStarted.Lazily, false)
-
-    @OptIn(ExperimentalCoroutinesApi::class)
-    private val hasTroubleConnectingFlow = vpnStatusProviderUI.uiStatus
+    val hasTroubleConnecting = vpnStatusProviderUI.uiStatus
         .distinctUntilChanged { old, new ->
             old.isActivelyEstablishingConnection() == new.isActivelyEstablishingConnection()
                 && old.connectIntent == new.connectIntent
@@ -115,6 +74,7 @@ class ChangeServerManager @Inject constructor(
                 emit(true)
             }
         }
+        .stateIn(mainScope, SharingStarted.Lazily, false)
 
     init {
         vpnStatusProviderUI.uiStatus
