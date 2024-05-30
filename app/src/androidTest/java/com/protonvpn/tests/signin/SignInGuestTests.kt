e2acb99ea9d1a79d0ccc04ffb73aacec9fba96ca
Mateusz Markowicz <mateusz.markowicz@proton.ch>
2024-05-30 13:15:27 +0200

    Firebase: test on API 34 device

--- app/src/androidTest/java/com/protonvpn/tests/signin/SignInGuestTests.kt
+++ app/src/androidTest/java/com/protonvpn/tests/signin/SignInGuestTests.kt
@@ -4,7 +4,9 @@ import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.filters.SdkSuppress
+import com.protonvpn.actions.HumanVerificationRobot
 import com.protonvpn.actions.OnboardingRobot
+import com.protonvpn.actions.SignupRobot
 import com.protonvpn.actions.compose.HomeRobot
 import com.protonvpn.actions.compose.SettingsRobot
 import com.protonvpn.actions.compose.interfaces.verify
@@ -14,9 +16,14 @@ import com.protonvpn.testsHelper.TestSetup
 import dagger.hilt.android.testing.HiltAndroidTest
 import me.proton.core.auth.test.MinimalSignInGuestTests
 import me.proton.core.auth.test.fake.FakeIsCredentialLessEnabled
+import me.proton.core.auth.test.robot.CredentialLessWelcomeRobot
+import me.proton.core.auth.test.robot.signup.SetPasswordRobot
+import me.proton.core.auth.test.robot.signup.SignUpRobot
 import me.proton.core.test.quark.Quark
+import me.proton.core.util.kotlin.random
 import me.proton.test.fusion.FusionConfig
 import org.junit.Rule
+import org.junit.Test
 import org.junit.rules.ExternalResource
 import org.junit.rules.RuleChain
 import org.junit.runner.RunWith
@@ -27,6 +34,9 @@ import javax.inject.Inject
 @SdkSuppress(minSdkVersion = 28) // HV robot doesn't work well on older Android versions
 @HiltAndroidTest
 class SignInGuestTests : MinimalSignInGuestTests {
+
+    private val humanVerificationRobot = HumanVerificationRobot()
+
     @get:Rule
     val rule: RuleChain = realBackendRule()
         .around(object : ExternalResource() {
@@ -43,6 +53,28 @@ class SignInGuestTests : MinimalSignInGuestTests {
     lateinit var isCredentialLessEnabled: FakeIsCredentialLessEnabled
 
     override val quark: Quark get() = TestSetup.quark
+
+    @Test
+    override fun credentialLessToRegularAccount() {
+        CredentialLessWelcomeRobot.clickContinueAsGuest()
+        verifyAfterCredentialLessSignup()
+
+        navigateToSignupFromCredentialLess()
+
+        val testUsername = "test-${String.random()}"
+        SignUpRobot.forExternal().clickSwitch()
+        SignUpRobot
+            .forInternal()
+            .fillUsername(testUsername)
+            .clickNext()
+        SetPasswordRobot
+            .fillAndClickNext("123123123")
+
+        SignupRobot().enterRecoveryEmail("${testUsername}@proton.ch")
+        humanVerificationRobot.verifyViaEmail()
+
+        verifyAfterRegularSignup(testUsername)
+    }
 
     override fun navigateToSignupFromCredentialLess() {
         HomeRobot
