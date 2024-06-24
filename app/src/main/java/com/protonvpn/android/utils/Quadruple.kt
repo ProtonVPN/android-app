112798b0dc4d7701136a0f5cf9ae63ae3b78fa8f
Marcin Simonides <marcin.simonides@proton.ch>
2024-06-24 16:21:55 +0200

    Don't reload app names on each split tunneling view update.

---
+++ app/src/main/java/com/protonvpn/android/utils/Quadruple.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2024. Proton AG
+ *
+ * This file is part of ProtonVPN.
+ *
+ * ProtonVPN is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonVPN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.protonvpn.android.utils
+
+data class Quadruple<T1, T2, T3, T4>(
+    val first: T1,
+    val second: T2,
+    val third: T3,
+    val fourth: T4
+)
