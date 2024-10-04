1a19838d55dd50b2cfeb7f532ea69428aed02f14
Marcin Simonides <marcin.simonides@proton.ch>
2024-10-04 10:46:31 +0200

    Fix bottom bar label alignment [VPNAND-1931].

--- app/src/main/java/com/protonvpn/android/redesign/main_screen/ui/BottomBarView.kt
+++ app/src/main/java/com/protonvpn/android/redesign/main_screen/ui/BottomBarView.kt
@@ -31,6 +31,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.compositeOver
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.protonvpn.android.R
@@ -81,6 +82,7 @@ fun BottomBarView(
                     Text(
                         text = label,
                         style = ProtonTheme.typography.captionStrongUnspecified,
+                        textAlign = TextAlign.Center
                     )
                 }
             )
