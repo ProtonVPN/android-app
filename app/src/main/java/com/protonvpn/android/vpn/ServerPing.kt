4645d4a8c66bfacb8fc5a490b9c1b2a0a2740392
Marcin Simonides <marcin.simonides@proton.ch>
2024-11-04 14:00:23 +0100

    Update devesecops kit.

--- app/src/main/java/com/protonvpn/android/vpn/ServerPing.kt
+++ app/src/main/java/com/protonvpn/android/vpn/ServerPing.kt
@@ -78,7 +78,7 @@ class ServerPing @Inject constructor(
     ): Boolean = withContext(dispatcherProvider.Io) {
         suspendCancellableCoroutine<Boolean> { continuation ->
             val result = try {
-                // nosemgrep: semgrep.gitlab.find_sec_bugs_kotlin.URLCONNECTION_SSRF_FD-1
+                // nosemgrep: gitlab.find_sec_bugs_kotlin.URLCONNECTION_SSRF_FD-1
                 val address = InetSocketAddress(InetAddress.getByName(ip), port)
                 val result = if (tcp)
                     pingTcp(pingData, address, continuation, timeout)
@@ -107,7 +107,7 @@ class ServerPing @Inject constructor(
         continuation: CancellableContinuation<*>,
         timeout: Int
     ): Boolean {
-        // nosemgrep: semgrep.kotlin.lang.security.unencrypted-socket.unencrypted-socket
+        // nosemgrep: kotlin.lang.security.unencrypted-socket.unencrypted-socket
         Socket().use { socket ->
             continuation.invokeOnCancellation {
                 socket.close()
@@ -150,7 +150,7 @@ class ServerPing @Inject constructor(
 
     private fun protectSocket(socket: Socket) {
         if (!socket.isBound) {
-            // nosemgrep: semgrep.gitlab.find_sec_bugs_kotlin.URLCONNECTION_SSRF_FD-1
+            // nosemgrep: gitlab.find_sec_bugs_kotlin.URLCONNECTION_SSRF_FD-1
             socket.bind(InetSocketAddress(0))
         }
         val success = try {
@@ -164,7 +164,7 @@ class ServerPing @Inject constructor(
 
     private fun protectSocket(socket: DatagramSocket) {
         if (!socket.isBound) {
-            // nosemgrep: semgrep.gitlab.find_sec_bugs_kotlin.URLCONNECTION_SSRF_FD-1
+            // nosemgrep: gitlab.find_sec_bugs_kotlin.URLCONNECTION_SSRF_FD-1
             socket.bind(InetSocketAddress(0))
         }
         val success = try {
