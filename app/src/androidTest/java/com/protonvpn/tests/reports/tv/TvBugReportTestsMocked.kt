a1914e9be4b50049d94fddfd74a3999103e63082
Alexander Talledo <alexander.talledo@proton.ch>
2025-11-19 11:27:59 +0100

    test(bug-report): Fix TvBugReportTestsMocked#sendBugReportSuccess flaky test

--- app/src/androidTest/java/com/protonvpn/tests/reports/tv/TvBugReportTestsMocked.kt
+++ app/src/androidTest/java/com/protonvpn/tests/reports/tv/TvBugReportTestsMocked.kt
@@ -22,20 +22,17 @@ package com.protonvpn.tests.reports.tv
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import com.protonvpn.android.models.config.bugreport.Category
 import com.protonvpn.android.models.config.bugreport.DynamicReportModel
+import com.protonvpn.android.models.config.bugreport.InputField
 import com.protonvpn.android.tv.reports.TvBugReportActivity
-import com.protonvpn.android.utils.FileUtils
-import com.protonvpn.android.utils.Storage
 import com.protonvpn.interfaces.verify
 import com.protonvpn.mocks.TestApiConfig
 import com.protonvpn.robots.tv.TvBugReportRobot
+import com.protonvpn.testRules.AppConfigRefreshTestRule
 import com.protonvpn.testRules.ProtonHiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import kotlinx.coroutines.runBlocking
-import kotlinx.serialization.builtins.ListSerializer
 import me.proton.core.network.data.protonApi.GenericResponse
 import me.proton.core.network.domain.ResponseCodes
 import me.proton.test.fusion.FusionConfig
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
@@ -46,9 +43,29 @@ class TvBugReportTestsMocked {
     private val activityComposeRule = createAndroidComposeRule<TvBugReportActivity>()
         .also(FusionConfig.Compose.testRule::set)
 
+    private val categories: List<Category> = listOf(
+        Category(
+            label = "Something else",
+            submitLabel = "categorySubmitLabel",
+            suggestions = emptyList(),
+            inputFields = listOf(
+                InputField(
+                    label = "What went wrong?",
+                    placeholder = "Describe the problem in as much detail as you can. If there was an error message, let us know what it said.",
+                    submitLabel = "inputFieldSubmitLabel",
+                    type = "TextMultiLine",
+                ),
+            ),
+        ),
+    )
+
     private val hiltRule = ProtonHiltAndroidRule(
         testInstance = this,
         apiConfig = TestApiConfig.Mocked {
+            rule(get, path eq "/vpn/v1/featureconfig/dynamic-bug-reports") {
+                respond(serializableObject = DynamicReportModel(categories = categories))
+            }
+
             rule(post, path eq "/core/v4/reports/bug") {
                 respond(serializableObject = GenericResponse(code = ResponseCodes.OK))
             }
@@ -58,20 +75,12 @@ class TvBugReportTestsMocked {
     @get:Rule
     val ruleChain: RuleChain = RuleChain
         .outerRule(hiltRule)
+        .around(AppConfigRefreshTestRule())
         .around(activityComposeRule)
-
-    private lateinit var categories: List<Category>
-
-    @Before
-    fun setUp() {
-        categories = Storage.load(DynamicReportModel::class.java)
-            ?.categories
-            .orEmpty()
-    }
 
     @Test
     fun sendBugReportSuccess() {
-        val categoryIndex = categories.size.minus(1)
+        val categoryIndex = 0
         val category = categories[categoryIndex]
 
         TvBugReportRobot
