758b56c55eec134205ba68374283a037493c8586
Marcin Simonides <marcin.simonides@proton.ch>
2026-03-18 11:57:55 +0100

    Revert "Disable HV tests temporarily."
    This reverts commit ff7ce90de9e5045aa7e31031a123808f269401cb.

--- app/src/androidTest/java/com/protonvpn/tests/login/mobile/LoginTestsBlack.kt
+++ app/src/androidTest/java/com/protonvpn/tests/login/mobile/LoginTestsBlack.kt
@@ -42,7 +42,6 @@ import me.proton.core.test.quark.Quark
 import me.proton.core.test.quark.data.User
 import me.proton.test.fusion.Fusion
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -101,7 +100,6 @@ class LoginTestsBlack {
     }
 
     @Test
-    @Ignore("Disabled until the test environment is fixed.")
     //Can't complete captcha on API23 due to animations bug in test framework.
     @SdkSuppress(minSdkVersion = 28)
     fun loginWithHumanVerification() {
