65dba4931d2ced11a6e37d90963ad1e909fcff80
Mateusz Markowicz <mateusz.markowicz@proton.ch>
2026-03-23 19:06:52 +0100

    Use kill-switch-type feature flag for skipping primary route [VPNCORE-52]

--- app/src/main/java/com/protonvpn/android/appconfig/usecase/ShouldSkipPrimaryApiRoute.kt
+++ app/src/main/java/com/protonvpn/android/appconfig/usecase/ShouldSkipPrimaryApiRoute.kt
@@ -21,24 +21,28 @@ package com.protonvpn.android.appconfig.usecase
 
 import com.protonvpn.android.appconfig.UserCountryPhysical
 import com.protonvpn.android.auth.usecase.CurrentUser
+import com.protonvpn.android.concurrency.VpnDispatcherProvider
 import com.protonvpn.android.di.ElapsedRealtimeClock
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.withContext
+import me.proton.core.featureflag.domain.ExperimentalProtonFeatureFlag
+import me.proton.core.featureflag.domain.FeatureFlagManager
 import me.proton.core.featureflag.domain.entity.FeatureId
-import me.proton.core.featureflag.domain.repository.FeatureFlagRepository
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.time.Duration.Companion.minutes
 
 private val SKIP_PRIMARY_API_ROUTE_COUNTRIES = setOf("RU", "IR", "MM", "CN")
-private val SKIP_PRIMARY_API_ROUTE_FOR_SELECTED_COUNTRIES_FEATURE_ID =
-    FeatureId("SkipPrimaryApiRouteForSelectedCountries")
+private val SKIP_PRIMARY_API_ROUTE_FOR_SELECTED_COUNTRIES_DISABLED_FEATURE_ID =
+    FeatureId("SkipPrimaryApiRouteForSelectedCountriesDisabled")
 
 @Singleton
 class ShouldSkipPrimaryApiRoute @Inject constructor(
     private val userCountry: UserCountryPhysical,
-    private val featureFlagRepository: dagger.Lazy<FeatureFlagRepository>,
+    private val featureFlagManager: dagger.Lazy<FeatureFlagManager>,
     private val currentUser: CurrentUser,
+    private val dispatcherProvider: VpnDispatcherProvider,
     @ElapsedRealtimeClock private val nowMs: () -> Long,
 ) {
     private var cachedValue: Boolean? = null
@@ -57,13 +61,17 @@ class ShouldSkipPrimaryApiRoute @Inject constructor(
         }
     }
 
+    @OptIn(ExperimentalProtonFeatureFlag::class)
     private suspend fun shouldSkip(): Boolean =
         userCountry()?.countryCode in SKIP_PRIMARY_API_ROUTE_COUNTRIES &&
-        // Use FeatureFlagRepository.getValue to get cached value and avoid making API calls to
-        // fetch the FF (this class is used during API call). We assume default value to be true as
-        // we need to make a decision before first API call is made.
-        featureFlagRepository.get().getValue(
-            currentUser.user()?.userId,
-            SKIP_PRIMARY_API_ROUTE_FOR_SELECTED_COUNTRIES_FEATURE_ID
-        ) != false
+        // Use FeatureFlagManager.getValue to get cached value and avoid making API calls to
+        // fetch the FF (this class is used during API call). We need to make a decision before
+        // the first API call is made, but once feature flags are refreshed, we check for
+        // kill-switch for this feature.
+        withContext(dispatcherProvider.Io) {
+            !featureFlagManager.get().getValue(
+                currentUser.user()?.userId,
+                SKIP_PRIMARY_API_ROUTE_FOR_SELECTED_COUNTRIES_DISABLED_FEATURE_ID
+            )
+        }
 }
